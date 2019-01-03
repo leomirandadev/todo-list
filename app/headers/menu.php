645075270ec6985cc8a1830f1a725de9f2a0288e
LeoMiranda <leoamiranda2@gmail.com>
2019-01-03 16:19:51 -0200

    inserção da logo no menu

--- app/headers/menu.php
+++ app/headers/menu.php
@@ -1,3 +1,6 @@
-<div class="col-md-12 right">
+<div class="col-sm-6 col-md-6">
+  <img src='<?=$config->urlLogo?>' height="60" >
+</div>
+<div class="col-sm-6 col-md-6 right">
   <a class='btn btn-danger' href="components/logoff.php"><i class="fas fa-power-off"></i> Logout</a>
 </div>
