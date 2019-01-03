0204601ce6fc99c6d536ea23fc35f7822dae022e
LeoMiranda <leoamiranda2@gmail.com>
2019-01-03 00:52:14 -0200

    model e funções task codificadas

---
+++ api/models/Task.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * Task Class Doc Comment
+ *
+ * @category Class
+ * @package  Task
+ * @author   Leonardo <leoamiranda2@gmail.com>
+ *
+ */
+
+namespace Models;
+use DataManager\DataCenter;
+
+class Task{
+
+	use DataCenter;
+	
+	public $id;
+	public $title;
+	public $description;
+	public $id_status;
+	public $created_on;
+	public $removed_on;
+	public $finished_on;
+	// last error from functions
+	public $lastError = "";
+
+//========================================================================================================
+//                                              GET TODAY DATE
+//========================================================================================================
+	
+	private function getTodayDate() {		
+		date_default_timezone_set("America/Sao_Paulo");
+		return date('Y-m-d');
+	}
+
+
+//========================================================================================================
+//                                              CREATE
+//========================================================================================================
+	
+	/**
+	 * login
+	 *
+	 * @return void
+	 */
+	public function new() {
+		if(
+			!isset($this->title) && empty($this->title) &&
+			!isset($this->description)
+		){
+			$lastError = array( 
+				'description' => "Não setada" ,
+				'title' => "Não setado ou vazio" 
+			);
+			return false;
+		}
+		$this->id_status = 1; // this is Id from "ativo" on dataBase		
+		$this->data = array(
+			"title" => $this->title,
+			"description" => $this->description,
+			"id_status" => $this->id_status
+		);
+
+		$idInserido = $this->Insert();
+
+		if(!$idInserido){
+			$lastError = "Ocorreu um erro ao inserir a task no banco de dados";
+			return false;	
+		}
+		return true;
+	}
+
+
+//========================================================================================================
+//                                              READ
+//========================================================================================================
+
+
+	/**
+	 * getAll
+	 *
+	 * @return void
+	 */
+	public function getAll() {
+		$this->condition = "all";
+			
+		$tasks = $this->GetBy();
+		if ( !$tasks ) return false;
+		
+		return $this->utf8_converter($tasks);
+	}
+
+
+	/**
+	 * getById
+	 *
+	 * @return void
+	 */
+	public function getById() {
+		if ( 
+			!isset($this->id) && empty($this->id)
+		) {
+			$lastError = "ID da task não foi informado";
+			return false;
+		}
+
+		$this->condition = array( "id" => $this->id );
+				
+		$task = $this->GetBy();
+
+		if ( !$task ) return false;
+		return $this->utf8_converter( $task[0] );
+	}
+
+
+//========================================================================================================
+//                                              UPDATE
+//========================================================================================================
+
+
+	/**
+	 * change
+	 *
+	 * @return void
+	 */
+	public function change() {
+		if(
+			!isset($this->id) && empty($this->id) &&
+			!isset($this->title) && empty($this->title) &&
+			!isset($this->description)
+		){
+			$lastError = array( 
+				'id' => "Não setada ou vazia",
+				'description' => "Não setada" ,
+				'title' => "Não setado ou vazio" 
+			);
+			return false;
+		}
+		
+		$this->id_status = 1; // this is Id from "ativo" on dataBase		
+		$this->data = array(
+			"title" => $this->title,
+			"description" => $this->description,
+			"id_status" => $this->id_status
+		);
+
+		$udapted = $this->Update();
+
+		if(!$udapted){
+			$lastError = "Ocorreu um erro ao inserir a task no banco de dados";
+			return false;	
+		}
+		return true;
+	}
+
+
+	/**
+	 * finished
+	 *
+	 * @return void
+	 */
+	public function finish() {
+		if ( 
+			!isset($this->id) && empty($this->id)
+		) {
+			$lastError = "ID da task não foi informado";
+			return false;
+		}
+
+		$this->finished_on = $this->getTodayDate();
+		$this->id_status = 2;
+		$this->data = array( "finished_on" => $this->finished_on, "id_status" => $this->id_status );
+
+		$udapted = $this->Update();
+
+		if(!$udapted){
+			$lastError = "Ocorreu um erro ao alterar a task no banco de dados";
+			return false;	
+		}
+		
+		return true;
+	}
+	
+	
+	/**
+	 * notFinish
+	 *
+	 * @return void
+	 */
+	public function notFinish() {
+		if ( 
+			!isset($this->id) && empty($this->id)
+		) {
+			$lastError = "ID da task não foi informado";
+			return false;
+		}
+
+		$this->id_status = 1;
+		$this->data = array( "finished_on" => null, "id_status" => $this->id_status );
+
+		$udapted = $this->Update();
+
+		if(!$udapted){
+			$lastError = "Ocorreu um erro ao alterar a task no banco de dados";
+			return false;	
+		}
+		
+		return true;
+	}
+	
+	
+	/**
+	 * remove
+	 *
+	 * @return void
+	 */
+	public function remove() {
+		if ( !isset($this->id) && empty($this->id) ) {
+			$lastError = "ID da task não foi informado";
+			return false;
+		}
+
+		$this->removed_on = $this->getTodayDate();
+		$this->data = array( "removed_on" => $this->removed_on );
+
+		$udapted = $this->Update();
+
+		if(!$udapted){
+			$lastError = "Ocorreu um erro ao alterar a task no banco de dados";
+			return false;	
+		}
+		
+		return true;
+	}
+
+
+}
+?>
